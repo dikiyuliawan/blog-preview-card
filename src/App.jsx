e122d3d21a76d02a64edc4b02e955404bfffedb7
dikiyuliawan <dikiyuliawan@gmail.com>
2024-02-04 00:15:48 +0700

    add chip, typography, and avatar on card content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import articleLogo from './assets/images/illustration-article.svg'
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
+import avatarImg from './assets/images/image-avatar.webp'
+import { Avatar, Card, CardActionArea, CardContent, CardMedia, Chip, Stack, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
@@ -7,12 +8,19 @@ function App() {
 
   return (
     <>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+    <Card sx={{ maxWidth: 345, padding: 1 }}>
+      <CardActionArea sx={{paddingLeft: 2, paddingRight: 2}}>
         <CardMedia component="img" height="140" image={articleLogo} />
       </CardActionArea>
       <CardContent>
-        <Typography variant='h5' component="div">HTML & CSS Foundations</Typography>
+        <Chip label="Learning" size='small' sx={{bgcolor: "#E3D026", borderRadius: 0, marginBottom: 1}} />
+        <Typography variant='body1' pb={1}>Published 18 February 2024</Typography>
+        <Typography gutterBottom variant="h5">HTML & CSS Foundations</Typography>
+        <Typography variant="body2" color="text.secondary">These languages are the backbone of every website, defining structure, content, and presentation</Typography>
+        <Stack direction="row" pt={2}>
+          <Avatar src={avatarImg}/>
+          <Typography variant='body3' sx={{padding: 1, fontWeight: 600}}>Greg Hooper</Typography>
+        </Stack>
       </CardContent>
     </Card>
     </>
